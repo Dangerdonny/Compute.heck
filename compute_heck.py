a7379588cdcd45cf79e7f943e351dfe3f15f77cb
Dangerdonny <90573817+Dangerdonny@users.noreply.github.com>
2022-03-17 10:35:06 -0700

    Add files via upload

---
+++ compute_heck.py
@@ -0,0 +1,25 @@
+import random
+import os
+import tkinter
+a = (random.randrange(1,100000000000000000000000000000000000000000000000000000000000000000000))
+b = (random.randrange(1,100000000000000000000000000000000000000000000000000000000000000000000))
+c = (a*b)
+d = (c*a)
+e = (a*b*c*d)
+f= (e/d)
+while True:
+    print(c*d , d/c, c+d+c*d, d*c*b*a,
+ "welcome to compute hech", 
+"ur pc is gonna die",
+"I'm doing ur mother", 
+f*f )
+
+
+
+
+
+
+
+
+
+
